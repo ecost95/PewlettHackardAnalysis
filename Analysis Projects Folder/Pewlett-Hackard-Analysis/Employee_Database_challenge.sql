a96c18da18fa8740220e3fdc983708b4f8523b55
Emily Costello <eccostello1@gmail.com>
2022-10-10 13:46:41 -0400

    adding mentorship eligibility data/query.

--- Analysis Projects Folder/Pewlett-Hackard-Analysis/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis/Employee_Database_challenge.sql
@@ -29,3 +29,20 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title 
 ORDER BY ut.count DESC;
+
+SELECT DISTINCT ON (e.emp_no) 
+                    e.emp_no, 
+                    e.first_name, 
+                    e.last_name, 
+                    e.birth_date, 
+                    ti.from_date,
+                    ti.to_date,
+                    ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+JOIN dept_emp as de ON e.emp_no = de.emp_no
+JOIN titles as ti ON e.emp_no = ti.emp_no
+WHERE ti.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
+
